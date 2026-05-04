64384067271fdca8e7c9e41924e8d5bfc4f94891
Chandail-Kajal <153283465+Chandail-Kajal@users.noreply.github.com>
2026-05-04 20:50:27 +0900

    footer xl and lg done

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,15 +16,15 @@ export type Section = {
 
 export function FooterColumn({ title, links }: Section) {
     return (
-        <div className="flex flex-col md:gap-4">
-            <h3 className="text-[#B9D531] font-semibold font-sohne-halbfett tracking-wider md:text-sm text-md">{title}</h3>
-            <div className="flex flex-col gap-2">
+        <div className="flex flex-col md:gap-4 xl:gap-8">
+            <h3 className="text-[#B9D531] font-semibold font-sohne-halbfett tracking-wider xl:text-2xl md:text-sm lg:text-md text-md">{title}</h3>
+            <div className="flex flex-col gap-2 xl:gap-3">
                 {links.map((item, index) => {
                     if (typeof item === "string") {
                         return (
                             <p
                                 key={index}
-                                className="text-gray-300 text-sm md:text-[13px] hover:text-white tracking-normal cursor-pointer"
+                                className="text-gray-300 text-sm xl:text-xl lg:text-[14px] md:text-[13px] hover:text-white tracking-normal cursor-pointer"
                             >
                                 {item}
                             </p>
@@ -32,10 +32,10 @@ export function FooterColumn({ title, links }: Section) {
                     }
                     return (
                         <div key={index} className="flex flex-col gap-0">
-                            <p className="text-gray-300 text-sm hover:text-white cursor-pointer font-sans">
+                            <p className="text-gray-300 xl:text-xl md:text-[13px] lg:text-[14px] text-sm hover:text-white cursor-pointer font-sans">
                                 {item.title}
                             </p>
-                            <span className="text-xs font-sans text-gray-300/70">
+                            <span className="text-xs font-sans xl:text-sm text-gray-300/70">
                                 {item.caption}
                             </span>
                         </div>
@@ -119,9 +119,7 @@ export default function Footer() {
             col: 2,
             title: "BULK ORDERS",
             links: [
-                "For gifting & corporate orders",
-                "WhatsApp us at +918904892616",
-                "bulk@movatobags.com",
+                "For gifting & corporate orders WhatsApp us at + 918904892616 bulk@movatobags.com",
             ],
         },
     ];
@@ -145,7 +143,7 @@ export default function Footer() {
     ];
 
     return (
-        <footer className="bg-[#304B39] text-white md:px-16 md:pt-14 md:pb-2 px-4 py-2 relative overflow-hidden " style={{ backgroundImage: 'url("/assets/Website-Footer.png")' }}>
+        <footer className="bg-[#304B39] text-white md:px-16 md:pt-14 md:pb-2 xl:pt-16 xl:pb-4 px-4 py-2 relative overflow-hidden " style={{ backgroundImage: 'url("/assets/Website-Footer.png")' }}>
             <div className="absolute inset-0 opacity-20 -z-10">
                 <Image
                     src="/assets/images/header-1.jpeg"
@@ -155,60 +153,61 @@ export default function Footer() {
                 />
             </div>
 
-            <div className="md:grid md:grid-cols-4 md:gap-8 flex flex-col relative">
-                <div className="flex flex-col gap-6 md:gap-4">
+            <div className="md:grid md:grid-cols-4 md:gap-8 lg:gap-8 flex flex-col relative">
+                <div className="flex flex-col gap-6 md:gap-4 xl:gap-10">
                     <div>
                         <Image
                             src="/assets/images/Frame-50.svg"
                             width={290}
                             height={60}
+                            // className="xl:h-22"
                             alt="footer"
                         />
-                        <p className="w-[95%] text-xs font-sans font-thin md:text-[13px] leading-tight text-white/80 mt-6 tracking-wide">
+                        <p className="w-[95%] text-xs font-sans font-thin xl:text-xl xl:text-white xl:font-thin md:text-[13px] leading-tight text-white/80 mt-6 tracking-wide">
                             Movato designs performance-driven
                             luggage for real travel, from daily work trips
                             to long journeys. Thoughtfully engineered. Rigorously tested.
                             Built to move with you—wherever life takes you.
                         </p>
-                        <h1 className="pt-3 leading-normal">#BuiltToPerform</h1>
+                        <h1 className="pt-3 leading-normal xl:text-2xl">#BuiltToPerform</h1>
                     </div>
 
 
-                    <div className="flex gap-3 md:mt-2">
-                        {Array.from({ length: 4 }).map((_, index) => <div key={`black_box_${index}`} className="h-12 md:h-14 aspect-square bg-[#2B2F33] rounded"></div>
+                    <div className="flex gap-3 md:mt-2 xl:mt-0">
+                        {Array.from({ length: 4 }).map((_, index) => <div key={`black_box_${index}`} className="h-12 xl:h-18 md:h-14 aspect-square bg-[#2B2F33] rounded"></div>
                         )}
                     </div>
 
 
-                    <div className="flex flex-col font-sans leading-normal md:mt-8 lg:gap-8 md:gap-3">
-                        <h3 className="text-[#B9D531] font-semibold text-md font-sohne-dreivierfett leading-tight ">
+                    <div className="flex flex-col font-sans leading-normal md:mt-8 lg:gap-3 md:gap-3">
+                        <h3 className="text-[#B9D531] font-medium text-md xl:text-2xl md:mb-2 font-sohne-dreivierfett leading-tight tracking-wider ">
                             CUSTOMER SUPPORT
                         </h3>
-                        <p className="text-xs text-gray-300 mt-2 lg:mt-4">
+                        <p className="text-xs xl:text-xl text-gray-300">
                             Call/WhatsApp: +91 99999 99999
                         </p>
-                        <p className="text-xs text-gray-300 lg:mt-4">
+                        <p className="text-xs xl:text-xl text-gray-300">
                             Email: support@movatobags.com
                         </p>
-                        <p className="text-xs text-gray-300 lg:mt-4">
+                        <p className="text-xs xl:text-xl text-gray-300">
                             Monday - Saturday / 10 AM - 7 PM
                         </p>
                     </div>
 
 
-                    <div className="font-sans md:mt-8">
-                        <h3 className="text-[#B9D531] font-semibold md:text-md font-sohne-dreivierfett lg:mt-10 ">
+                    <div className="font-sans md:mt-8 lg:mt-0">
+                        <h3 className="text-[#B9D531] xl:text-2xl font-medium md:text-md font-sohne-dreivierfett tracking-wider lg:mt-10 ">
                             STAY UPDATED
                         </h3>
-                        <div className="flex mt-2 bg-white lg:mt-4">
+                        <div className="flex mt-2 bg-white xl:mt-7 lg:mt-4">
                             <input
                                 type="email"
                                 placeholder="Enter your email"
-                                className="px-3 py-2 text-black text-sm w-full"
+                                className="px-3 py-2 xl:p-4 text-black text-sm w-full"
                             />
                             <button className="bg-[#2F392D] px-4">→</button>
                         </div>
-                        <p className="mt-6 text-xs md:text-[11px] font-sans font-thin text-white/70">
+                        <p className="mt-6 text-xs md:text-[11px] xl:text-base xl:leading-tight font-sans font-thin text-white/70">
                             Product launches, performance insights, and travel stories —
                             straight from the Movato team. No spam. No noise.Just things worth knowing.
                         </p>
@@ -236,7 +235,7 @@ export default function Footer() {
             </div>
 
 
-            <div className="border-t border-gray-600 mt-10 md:mt-4 md:pt-4 text-center text-sm text-gray-400">
+            <div className="border-t border-gray-600 mt-10 xl:text-lg xl:pt-6 md:mt-4 md:pt-4 text-center text-sm text-gray-400">
                 © 2026 MOVATO. All rights reserved.
             </div>
         </footer>
